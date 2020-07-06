af19888a254071e54d85b27a3ed363da2f44a32d
Milena <bobkova.milena25@gmail.com>
2020-07-06 12:30:38 +0300

    Создает модуль активной/неактивной страницы

---
+++ js/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function () {
+  var map = document.querySelector('.map');
+  var adForm = document.querySelector('.ad-form');
+  var mapFilter = map.querySelector('.map-filters');
+  var mapFiltersContainer = map.querySelector('.map__filters-container');
+  var adFormFieldsets = adForm.querySelectorAll('fieldset');
+  var pinMain = document.querySelector('.map__pin--main');
+
+  var pageActive = function () {
+    map.classList.remove('map--faded');
+    adForm.classList.remove('ad-form--disabled');
+
+    pinMain.removeEventListener('mousedown', window.coords.pinMainMouseDownHandler);
+
+    window.form.active();
+  };
+
+  var pageInactive = function () {
+    adForm.reset();
+
+    adForm.classList.add('ad-form--disabled');
+    window.form.changeFieldsets(adFormFieldsets);
+    mapFiltersContainer.style.display = 'none';
+
+    window.map.removePins();
+    window.map.removeCards();
+    map.classList.add('map--faded');
+    pinMain.addEventListener('mousedown', window.coords.pinMainMouseDownHandler);
+    pinMain.style.top = window.data.pinMainStartCoords.TOP;
+    pinMain.style.left = window.data.pinMainStartCoords.LEFT;
+  };
+
+  window.main = {
+    pageActive: pageActive,
+    pageInactive: pageInactive
+  };
+})();
